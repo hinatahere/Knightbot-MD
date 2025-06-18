659e1e41a18f9f0859fca485723738db090dd6ec
hinatahere <hinatauzuumaki447@gmail.com>
2025-06-18 18:47:55 +0000

    Update main.js

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ const imagineCommand = require('./commands/imagine');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
+global.channelLink = "https://whatsapp.com/channel/0029Vb5kgfIDDmFPET2QDP0p";
 global.ytch = "Mr Unique Hacker";
 
 // Add this near the top of main.js with other global configurations
